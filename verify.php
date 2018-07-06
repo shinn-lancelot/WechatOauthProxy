d09acbc7cd5e4d0cbd85f5284c79f54677fe1cde
shinn lancelot <945226793@qq.com>
2018-07-06 17:42:09 +0800

    1.添加微信授权回调域名的验证文件添加功能

---
+++ verify.php
@@ -0,0 +1,24 @@
+<?php
+
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
+    $res['code'] = -1001;
+    $res['message'] = '提交失败';
+
+    $filePrefix = 'MP_verify_';
+    $callBackUrl = $_SERVER['HTTP_HOST'];
+
+    $txt = strip_tags(trim($_POST['txt']));
+
+    if (empty($txt)) {
+        echo json_encode($res);
+        exit();
+    }
+
+    file_put_contents('./' . $filePrefix . $txt . '.txt' , $txt);
+    $res['code'] = 1001;
+    $res['message'] = '提交成功';
+    $res['data'] = array(
+        'callBackUrl'=>$callBackUrl
+    );
+    echo json_encode($res);
+}
